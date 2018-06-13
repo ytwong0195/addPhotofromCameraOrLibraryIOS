3b905c0c01ee0bd85f8d2686cc86fa73d2a0c367
ytwong0195 <ytwong0195@gmail.com>
2018-06-12 18:02:27 -0700

    second commit

---
+++ accessCameraPhotoLibrary/AddPhotoViewController.swift
@@ -0,0 +1,96 @@
+//
+//  ViewController.swift
+//  accessCameraPhotoLibrary
+//
+//  Created by .  Wong Ying Tung on 6/12/18.
+//  Copyright © 2018 ywong. All rights reserved.
+//
+
+import UIKit
+import MobileCoreServices
+import AVFoundation
+import Photos
+
+class AddPhotoViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    var newPic : Bool?
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var addPicLabel: UILabel!
+    @IBOutlet weak var addPicButton: UIButton!
+    
+    @IBAction func addButtonTapped(_ sender: Any) {
+        let pickImageAlert = UIAlertController(title: "Select Image From", message: "", preferredStyle: .actionSheet)
+        let cameraAction = UIAlertAction(title: "Camera", style: .default) {(action) in
+            if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera){
+                let imagePicker = UIImagePickerController()
+                imagePicker.delegate = self
+                imagePicker.sourceType = UIImagePickerControllerSourceType.camera
+                imagePicker.mediaTypes = [kUTTypeImage as String]
+                imagePicker.allowsEditing = false
+                self.present(imagePicker,animated: true,completion: nil)
+                self.newPic = true
+            }
+        }
+        let cameraRollAction = UIAlertAction(title:"Camera Roll", style:.default) {(action) in
+            if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.photoLibrary){
+                let imagePicker = UIImagePickerController()
+                imagePicker.delegate = self
+                imagePicker.sourceType = UIImagePickerControllerSourceType.photoLibrary
+                imagePicker.mediaTypes = [kUTTypeImage as String]
+                imagePicker.allowsEditing = false
+                self.present(imagePicker,animated: true,completion: nil)
+                self.newPic = false
+            }
+        }
+        pickImageAlert.addAction(cameraAction)
+        pickImageAlert.addAction(cameraRollAction)
+        self.present(pickImageAlert, animated: true)
+        
+        
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let mediaType = info[UIImagePickerControllerMediaType] as! NSString
+        if mediaType.isEqual(to: kUTTypeImage as String){
+            let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+            imageView.image = image
+            
+            if newPic == true {
+                UIImageWriteToSavedPhotosAlbum(image, self, #selector(imageError),nil)
+                
+            }
+        }
+        addPicLabel.isHidden = true
+        addPicButton.alpha = 0.5
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @objc func imageError(image: UIImage, didFinishSavingWithError error: NSErrorPointer, contextInfo: UnsafeRawPointer){
+        if error != nil {
+            let alert = UIAlertController(title: "Save Failed", message: "Failed to save image", preferredStyle: .alert)
+            let cancelAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alert.addAction(cancelAction)
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        self.dismiss(animated: true, completion: nil)
+        
+    }
+
+}
+
